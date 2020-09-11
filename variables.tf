a911dc2a3b59a163c941b7cc07511c90e4f83fd2
innfi <innfi.world@gmail.com>
2020-09-11 20:11:38 +0900

    additional subnet for multi az

--- variables.tf
+++ variables.tf
@@ -16,7 +16,8 @@ variable "vpc_azs" {
     description = "AZs"
     type = list 
     default =  [
-        "ap-northeast-2a"    
+        "ap-northeast-2a",
+        "ap-northeast-2c"
     ]
 }
 
@@ -24,7 +25,8 @@ variable "vpc_public_subnets" {
     description = "Public Subnets"
     type = list(string) 
     default = [
-        "10.0.1.0/24"
+        "10.0.1.0/24",
+        "10.0.2.0/24"
     ]
 }
 
