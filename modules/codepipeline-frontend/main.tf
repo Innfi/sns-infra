e24f0f6ab48391ea648b7c1c40a80052bdee3714
innfi <innfi.world@gmail.com>
2021-04-03 17:38:07 +0900

    codepipeline: fixed source stage

--- modules/codepipeline-frontend/main.tf
+++ modules/codepipeline-frontend/main.tf
@@ -17,65 +17,63 @@ resource "aws_s3_bucket" "sns-v2" {
 resource "aws_iam_role" "codepipeline_role" {
   name = "codepipeline_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service" : [
-          "codebuild.amazonaws.com",
-          "codedeploy.amazonaws.com", 
-          "codepipeline.amazonaws.com",
-          "codestar.amazonaws.com"
-        ]
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+          "Service" : [
+            "cloudwatch.amazonaws.com",
+            "events.amazonaws.com",
+            "codebuild.amazonaws.com",
+            "codedeploy.amazonaws.com", 
+            "codepipeline.amazonaws.com",
+            "codestar.amazonaws.com"
+          ]
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy" "codepipeline_role_policy" {
     name = "codepipeline_role_policy"
     role = aws_iam_role.codepipeline_role.id
 
-    policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-      {
-        "Action": [
-          "s3:*"
-        ],
-        "Effect": "Allow",
-        "Resource": [
-          "${aws_s3_bucket.sns-v2.arn}",
-          "${aws_s3_bucket.sns-v2.arn}/*"
-        ]
-      },
-      {
-        "Action": [
-          "codebuild:*",
-          "codedeploy:*",
-          "codepipeline:*",
-          "codestar:*",
-          "codestar-connections:*",
-          "logs:"
-        ],
-        "Effect": "Allow",
-        "Resource": "*"
-      }
-  ]
-}
-EOF
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+            "Action": [
+              "s3:*"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+              aws_s3_bucket.sns-v2.arn,
+              "${aws_s3_bucket.sns-v2.arn}/*"
+            ]
+          },
+          {
+            "Action": [
+              "codebuild:*",
+              "codedeploy:*",
+              "codepipeline:*",
+              "codestar:*",
+              "codestar-connections:*",
+              "logs:"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+          }
+      ]
+    })
 }
 
 resource "aws_codestarconnections_connection" "connection_github" {
-  name = "connection_github" 
+  name = "connection_github2"
   provider_type = "GitHub"
 }
 
@@ -127,7 +125,7 @@ resource "aws_codepipeline" "codepipeline_frontend" {
 
       configuration = {
         ConnectionArn = aws_codestarconnections_connection.connection_github.arn
-        FullRepositoryId = "https://github.com/innfi/snd-frontend"
+        FullRepositoryId = "Innfi/sns-frontend"
         BranchName = "main"
       }
     }
